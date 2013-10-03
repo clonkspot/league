832f902cbc04a6258052fe036e12f1bbe871c976
Sven Eberhardt <sven2@goldwipf.de>
2013-10-03 18:40:17 +0200

    Revert config to empty default.
    I hope noone logged into my test DB now ;)

--- config.php
+++ config.php
@@ -1,9 +1,9 @@
 <?php
 
-$webroot = 'D:/www/league2/';
+$webroot = '/what/where/Sites/league2/';
 
-require_once('smarty/libs/SmartyBC.class.php');
-$smarty = new SmartyBC();
+require_once($webroot.'smarty/libs/Smarty.class.php');
+$smarty = new Smarty();
 
 $smarty->template_dir = $webroot.'template/';
 $smarty->compile_dir = $webroot.'template_c/';
@@ -12,13 +12,13 @@ $smarty->config_dir = $webroot.'configs/';
 
 unset($webroot);
 
-$database = & new database('127.0.0.1','league2','CC67eWMqKcsHJ8UG','league2');
+$database = & new database('127.0.0.1','league2','league2','league2');
 
-$debug = FALSE;
-$debug_xml_log = FALSE;
-$debug_sql_slow_log = FALSE;
+$debug = TRUE;
+$debug_xml_log = TRUE;
+$debug_sql_slow_log = TRUE;
 $debug_skip_backend_checksum = TRUE;
-$debug_skip_flood_protection = TRUE;
+$debug_skip_flood_protection = FALSE;
 $debug_skip_session_path = TRUE;
 $debug_skip_resource_checksum = TRUE;
 
