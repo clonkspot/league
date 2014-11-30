5ad513e4b48809e15a2e48cc98e1ac996124fe12
Luchs <Luchs@euirc.eu>
2014-11-30 16:24:51 +0100

    game_events.php: Add init event with all active games

--- game_events.php
+++ game_events.php
@@ -2,6 +2,13 @@
 
 require_once('redis_config.php');
 
+function game_to_json($game_id, $mget) {
+    list($game, $game_reference) = $mget;
+    if (empty($game)) $game = 'null';
+    if (empty($game_reference)) $game_reference = 'null';
+    return '{"id":'.$game_id.',"game":'.$game.',"reference":'.$game_reference.'}';
+}
+
 header("Content-Type: text/event-stream\n\n");
 // Don't stop the script after 30s.
 set_time_limit(0);
@@ -9,6 +16,23 @@ set_time_limit(0);
 // We need two clients as we want to query Redis during pubsub.
 $psclient = create_redis_client();
 $redis = create_redis_client();
+
+// Fist, publish all currently active games.
+$active_game_ids = $redis->smembers('league:active_games');
+$active_games = $redis->pipeline(function($pipe) use ($active_game_ids) {
+    foreach ($active_game_ids as $game_id) {
+	$pipe->mget(array("league:game:$game_id", "league:game_reference:$game_id"));
+    }
+});
+echo "event: init\n";
+echo 'data: [';
+echo implode(',', array_map('game_to_json', $active_game_ids, $active_games));
+echo "]\n\n";
+
+ob_flush();
+flush();
+unset($active_games);
+unset($active_game_ids);
 
 $pubsub = $psclient->pubSubLoop();
 $pubsub->psubscribe('league:game:*');
@@ -22,12 +46,10 @@ foreach ($pubsub as $message) {
 			// Skip league:game:
 			$type = substr($message->channel, 12);
 			$game_id = $message->payload;
-			list($game, $game_reference) = $redis->mget(array("league:game:$game_id", "league:game_reference:$game_id"));
-			if (empty($game)) $game = 'null';
-			if (empty($game_reference)) $game_reference = 'null';
+			$mget = $redis->mget(array("league:game:$game_id", "league:game_reference:$game_id"));
 
 			echo "event: $type\n";
-			echo 'data: {"id":'.$game_id.',"game":'.$game.',"reference":'.$game_reference."}\n\n";
+			echo 'data: ' . game_to_json($game_id, $mget) . "\n\n";
 
 			ob_flush();
 			flush();
