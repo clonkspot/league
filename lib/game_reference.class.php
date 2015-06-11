4d3abaefe5a62efb9eaf8d1af3b3a9667867b1f8
Lukas Werling <lukas.werling@gmail.com>
2015-06-11 20:18:57 +0200

    Add game.to_json method

--- lib/game_reference.class.php
+++ lib/game_reference.class.php
@@ -1,5 +1,7 @@
 <?php
 
+include_once('game_reference_utils.inc.php');
+
 class game_reference
 {
 	var $data;
@@ -305,66 +307,5 @@ class game_reference
 
 }
 
-	function base64_encode_multi(&$val) 
-	{
-	   if (is_array($val)) 
-			array_walk($val,'base64_encode_multi');
-	   else 
-	   {
-	     $val = base64_encode($val);
-	   }
-	}
-
-	function base64_decode_multi(&$val) 
-	{
-	   if (is_array($val)) 
-			array_walk($val,'base64_decode_multi');
-	   else 
-	   {
-	     $val = base64_decode($val);
-	   }
-	}
-	
-	//from http://de.php.net/array
-	function array_deep_copy (&$array, &$copy) {
-	   if(!is_array($copy)) $copy = array();
-	   if(is_array($array))
-	   {
-		   foreach($array as $k => $v) {
-	       if(is_array($v)) {
-	           array_deep_copy($v,$copy[$k]);
-	       } else {
-	           $copy[$k] = $v;
-	       }
-		   }
-	   }
-	}
-	
-	function remove_quotes($string)
-	{
-		if($string[0] == '"' && $string[strlen($string)-1] == '"')
-		{
-			$string = substr($string,1,strlen($string)-2);
-		}
-		return $string;
-	}
-	
-	
-	function decode_octal($string)
-	{
-		$string = preg_replace_callback('/(^|[^\\\])\\\([0-9]+)/m', function($m) {
-			return $m[1] . chr(octdec($m[2]));
-		}, $string);
-		$string = str_replace('\\\\','\\',$string);
-		return $string;
-	}
-	function decode_octal_array(&$a, $index)
-	{
-		for($i=0;$i<count($a);$i++)
-		{
-			$a[$i][$index] = decode_octal($a[$i][$index]);
-			
-		}
-	}
 
 ?>
---
+++ lib/game_reference_format.class.php
@@ -0,0 +1,16 @@
+<?php
+
+include_once('game_reference_utils.inc.php');
+
+// Fixes formatting quirks from the game reference format.
+class game_reference_format {
+    static function string($str) {
+        $str = decode_octal(remove_quotes($str));
+        return mb_convert_encoding($str, 'UTF-8', 'ISO-8859-1');
+    }
+
+    static function number($str) {
+        return intval($str, 10);
+    }
+
+}
