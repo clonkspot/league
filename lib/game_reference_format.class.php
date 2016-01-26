75902cb1b9ea9ebae27cb108d1233eafaa840a34
Lukas Werling <lukas.werling@gmail.com>
2016-01-26 20:27:54 +0100

    Fix JSON encoding of umlauts
    OC (thankfully) doesn't use Latin1 anymore, so converting to UTF-8
    breaks the string.

--- lib/game_reference_format.class.php
+++ lib/game_reference_format.class.php
@@ -5,8 +5,7 @@ include_once('game_reference_utils.inc.php');
 // Fixes formatting quirks from the game reference format.
 class game_reference_format {
     static function string($str) {
-        $str = decode_octal(remove_quotes($str));
-        return mb_convert_encoding($str, 'UTF-8', 'ISO-8859-1');
+        return decode_octal(remove_quotes($str));
     }
 
     static function number($str) {
