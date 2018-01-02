fee999597d38e9f0966a24fe35270442de2b2e7a
Lukas Werling <lukas.werling@gmail.com>
2018-01-02 18:06:19 +0100

    Add Prometheus metrics endpoint

---
+++ metrics.php
@@ -0,0 +1,44 @@
+<?php
+
+// Exports metrics from lg_debug_counters in the Prometheus text format.
+
+require_once('lib/database.class.php');
+require_once('config.php');
+
+// value > 0 filters the 'dummy' entry which forces a new revision.
+$counters = $database->get_array("SELECT name, UNIX_TIMESTAMP(last_update)*1000 as timestamp, value*mean_duration AS duration FROM lg_debug_counter
+	WHERE revision = (SELECT MAX(revision) FROM lg_debug_counter) AND value > 0
+	ORDER BY name");
+
+header('Content-Type: text/plain; version=0.0.4');
+
+$type_seen = array();
+function print_counter($name, $labels, $duration, $timestamp)
+{
+	global $type_seen;
+	$lstr = '';
+	if (count($labels))
+	{
+		$lstr = '{'. implode(',', array_map(function($k, $v) { return $k.'="'. addcslashes($v, "\"\\\n") .'"'; }, array_keys($labels), $labels)) .'}';
+	}
+	$metric_name = "league_{$name}_duration_seconds_total";
+	if (!in_array($metric_name, $type_seen))
+	{
+		echo "\n# TYPE $metric_name counter\n";
+		$type_seen[] = $metric_name;
+	}
+	echo "$metric_name$lstr $duration $timestamp\n";
+}
+
+foreach ($counters as $counter)
+{
+	$labels = array();
+	$name = $counter["name"];
+	$name_parts = explode("_", $name, 2);
+	if (count($name_parts) == 2 && in_array($name_parts[0], array("site", "action")))
+	{
+		$name = $name_parts[0];
+		$labels[$name] = $name_parts[1];
+	}
+	print_counter($name, $labels, $counter["duration"], $counter["timestamp"]);
+}
